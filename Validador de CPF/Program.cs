fc5cdeb9815ffccca31216f573b429fa1893556e
Álvaro Mendes <alvaromendes2005@gmail.com>
2024-09-15 16:58:25 -0300

    att

--- Validador de CPF/Program.cs
+++ Validador de CPF/Program.cs
@@ -1,65 +1,58 @@
 ﻿using System.Text;
 
-Console.Write("Enter a CPF in the format (xxx.xxx.xxx-xx): ");
-string? cpfInput = Console.ReadLine();
-
-
-static bool IsCnpjValid(string? cnpj)
+internal class Program
 {
-    if (string.IsNullOrWhiteSpace(cnpj))
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Invalid CNPJ!");
-        return false;
+        Console.Write("Enter a CPF in the format (xxx.xxx.xxx-xx): ");
+        string? cpfInput = Console.ReadLine();
+
+        if (IsCpfValid(cpfInput))
+            Console.WriteLine("CPF is valid.");
+        else
+            Console.WriteLine("CPF is invalid.");
     }
 
+    static bool IsCpfValid(string? cpf)
+    {
+        if (string.IsNullOrWhiteSpace(cpf))
+        {
+            Console.WriteLine("Invalid CPF!");
+            return false;
+        }
 
-    return true;
+        StringBuilder formattedCpf = new();
+        foreach (var character in cpf)
+        {
+            if (char.IsDigit(character))
+                formattedCpf.Append(character);
+        }
+
+        if (formattedCpf.Length != 11)
+            return false;
+
+        char[] cpfArray = formattedCpf.ToString().ToCharArray();
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            sum += (cpfArray[i] - '0') * (10 - i);
+        }
+        int firstDigit = sum % 11 < 2 ? 0 : 11 - sum % 11;
+
+        if (firstDigit != cpfArray[9] - '0')
+            return false;
+
+        sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            sum += (cpfArray[i] - '0') * (11 - i);
+        }
+        int secondDigit = sum % 11 < 2 ? 0 : 11 - sum % 11;
+
+        if (secondDigit != cpfArray[10] - '0')
+            return false;
+
+        return true;
+    }
 }
-
-static bool IsCpfValid(string? cpf)
-{
-    if (string.IsNullOrWhiteSpace(cpf))
-    {
-        Console.WriteLine("Invalid CPF!");
-        return false;
-    }
-
-    StringBuilder formattedCpf = new();
-    foreach (var character in cpf)
-    {
-        if (char.IsDigit(character))
-            formattedCpf.Append(character);
-    }
-
-    if (formattedCpf.Length != 11)
-        return false;
-
-    char[] cpfArray = formattedCpf.ToString().ToCharArray();
-
-    int sum = 0;
-    for (int i = 0; i < 9; i++)
-    {
-        sum += (cpfArray[i] - '0') * (10 - i);
-    }
-    int firstDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-
-    if (firstDigit != (cpfArray[9] - '0'))
-        return false;
-
-    sum = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        sum += (cpfArray[i] - '0') * (11 - i);
-    }
-    int secondDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-
-    if (secondDigit != (cpfArray[10] - '0'))
-        return false;
-
-    return true;
-}
-
-if (cpfInput != null && IsCpfValid(cpfInput))
-    Console.WriteLine("CPF is valid.");
-else
-    Console.WriteLine("CPF is invalid.");
